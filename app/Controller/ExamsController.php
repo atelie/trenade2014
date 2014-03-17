389ceecb140dd89a10aa777ed97227ee11c4d7da
Atelie PC2 <atelie.duvidas@gmail.com>
2014-03-17 17:15:09 -0300

    Adição do random na Controller de Simulados

--- app/Controller/ExamsController.php
+++ app/Controller/ExamsController.php
@@ -1,7 +1,7 @@
 <?php
 	class ExamsController extends AppController{
 
-        public $uses = array('User', 'Exam', 'Course');
+        public $uses = array('User', 'Exam', 'Course', 'AltQuestion', 'TextQuestion');
 
         public function index(){
             $this->set('courses', array('[Selecione]') + $this->Course->find('list'));
@@ -9,11 +9,26 @@
 
         public function exam() {
             if ($this->request->is('post')) {
+                
                 $id_busca = $this->request->data['Exams']['course_id'];
+                
                 $course = $this->Course->find('first', 
                     array( 'conditions' => array('Course.id' => $id_busca)));
                 $course_name = $course['Course']['name'];
                 $this->set('nome_curso', $course_name);
+
+                
+
+                $this->set('random_questions', $this->AltQuestion->find('all', array(
+                //'fields' => array('DISTINCT altquestion_id'),
+                'conditions' => array('AltQuestion.course_id' => $id_busca), 
+                'order' => 'rand()',
+                'limit' => 15
+                )));
+
+                
+
+
             }else {
                 $this->redirect(array('action' => 'index'));
             }
--- app/View/Exams/exam.ctp
+++ app/View/Exams/exam.ctp
@@ -20,6 +20,22 @@
 		</div>
 		<div class="content">		
 			<h1>Simulado de <?php echo $nome_curso;?></h1>
+
+			<?php
+				$numeroQ = 0;
+				foreach ($random_questions as $random_question) {
+					$numeroQ++;
+					echo 
+					'<br><br><br>'.$numeroQ.') '.$pergunta = $random_question['AltQuestion']['question_text'].
+					'<br><br>A) '.$respostaA = $random_question['AltQuestion']['answerA'].
+					'<br>B) '.$respostaB = $random_question['AltQuestion']['answerB'].
+					'<br>C) '.$respostaC = $random_question['AltQuestion']['answerC'].
+					'<br>D) '.$respostaD = $random_question['AltQuestion']['answerD'].
+					'<br>E) '.$respostaE = $random_question['AltQuestion']['answerE'];
+				}
+
+			?>
+            
 		</div>
 	</div>
 </body>
